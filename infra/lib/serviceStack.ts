a6d99a3d2aff3378f8d3f209670780001a137411
Xinyue Li <tony.li.xinyue@outlook.com>
2026-01-25 20:33:19 -0800

    Add Cognito user pool for authentication

--- infra/lib/serviceStack.ts
+++ infra/lib/serviceStack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
 export class ServiceStack extends cdk.Stack {
   public readonly appTable: dynamodb.Table;
   public readonly cacheTable: dynamodb.Table;
+  public readonly userPool: cognito.UserPool;
+  public readonly userPoolClient: cognito.UserPoolClient;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -46,6 +49,55 @@ export class ServiceStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'CacheTableName', {
       value: this.cacheTable.tableName,
       description: 'DynamoDB table for cached external API data',
+    });
+
+    this.userPool = new cognito.UserPool(this, 'UserPool', {
+      userPoolName: 'phantom-users',
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      mfa: cognito.Mfa.OPTIONAL,
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+      customAttributes: {
+        preferredUsername: new cognito.StringAttribute({ mutable: true }),
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    this.userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
+      userPool: this.userPool,
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+      },
+      generateSecret: false,
+      preventUserExistenceErrors: true,
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: this.userPool.userPoolId,
+      description: 'Cognito User Pool ID',
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: this.userPoolClient.userPoolClientId,
+      description: 'Cognito User Pool Client ID',
     });
   }
 }
